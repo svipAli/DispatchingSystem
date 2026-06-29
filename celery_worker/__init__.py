8bc693f14b0f38145b115f842822103dca7185d3
Ali <svip_ali@foxmail.com>
2026-06-29 10:23:18 +0800

    移除未使用的 Celery 代码

--- celery_worker/__init__.py
+++
@@ -1,3 +0,0 @@
-from celery_worker.celery_app import celery_app
-
-__all__ = ["celery_app"]
--- celery_worker/tasks/__init__.py
+++
@@ -1,4 +0,0 @@
-from celery_worker.tasks.mcp_dispatch import dispatch_task, run_dispatch
-from celery_worker.tasks.periodic import check_node_health, check_user_expiry
-
-__all__ = ["dispatch_task", "run_dispatch", "check_node_health", "check_user_expiry"]
